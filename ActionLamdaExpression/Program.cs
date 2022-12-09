ec4b2bf49e483287ddee5ee9886650165f76c917
abdul.kr <abdul.kr@PIA3400>
2022-12-09 11:59:01 +0530

    Add project files.

---
+++ ActionLamdaExpression/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ActionLamdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, string> concatAction = (firstName, lastName) =>
+            Console.WriteLine($"\n Welcome {firstName + " " + lastName}"); ;
+            Action<int, int> sumAction = (firstNum, secondNum) =>
+            Console.WriteLine($"\n Sum of {firstNum} and {secondNum} is : {(firstNum + secondNum)}");
+            Action<int, int> diffAction = (firstNum, secondNum) =>
+            Console.WriteLine($"\n Difference of {firstNum} and {secondNum} is : {firstNum - secondNum}");
+            Action<int, int> proAction = (firstNum, secondNum) =>
+            Console.WriteLine($"\n Product of {firstNum} and {secondNum} is : {firstNum * secondNum}");
+            Action<int, int> divAction = (firstNum, secondNum) =>
+            {
+                if (secondNum != 0)
+                    Console.WriteLine($"\n Reminder of {firstNum} and {secondNum} is : {firstNum / secondNum}");
+                else
+                    Console.WriteLine($"\n Please enter non zero value for find remainder");
+
+            };
+
+            concatAction("Abdul", "Kadar");
+            sumAction(180, 65);
+            diffAction(98, 52);
+            proAction(82, 4);
+            divAction(80, 0);
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ LamdaExpression/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FuncLamdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, string> concatFunc = (firstName, lastName) =>
+            firstName + " " + lastName;
+            Func<int, int, int> sumFunc = (firstNum, secondNum) =>
+            firstNum + secondNum;
+            Func<int, int, int> difFunc = (firstNum, secondNum) =>
+            firstNum - secondNum;
+            Func<int, int, int> proFunc = (firstNum, secondNum) =>
+            firstNum * secondNum;
+            Func<int, int, int> divFunc = (firstNum, secondNum) =>
+            firstNum / secondNum;
+
+            Console.WriteLine($"\n Welcome {concatFunc("Abdul", "Kadar")}");
+            CalculateAdd(sumFunc, difFunc, proFunc, divFunc, 180, 95);
+
+            //CalculateSub(difFunc, 56, 19);
+            //CalculateMul(proFunc, 65, 8);
+            //CalculateDiv(divFunc, 400, 4);
+
+            Console.ReadKey();
+
+        }
+        static void CalculateAdd(Func<int, int, int> addfunc, Func<int, int, int> subFunc, Func<int, int, int> mulFunc, Func<int, int, int> divFunc, int firstNum, int secondNum)
+        {
+            Console.WriteLine($"\n Sum of {firstNum} and {secondNum} is : {addfunc(firstNum, secondNum)}");
+            Console.WriteLine($"\n Difference of {firstNum} and {secondNum} is : {subFunc(firstNum, secondNum)}");
+            Console.WriteLine($"\n Product of {firstNum} and {secondNum} is : {mulFunc(firstNum, secondNum)}");
+            Console.WriteLine($"\n Reminder of {firstNum} and {secondNum} is : {divFunc(firstNum, secondNum)}");
+        }
+        //static void CalculateSub(Func<int, int, int> subFunc, int firstNum, int secondNum)
+        //{
+        //    Console.WriteLine($"\n Difference of {firstNum} and {secondNum} is : {subFunc(firstNum, secondNum)}");
+        //}
+        //static void CalculateMul(Func<int, int, int> mulFunc, int firstNum, int secondNum)
+        //{
+        //    Console.WriteLine($"\n Product of {firstNum} and {secondNum} is : {mulFunc(firstNum, secondNum)}");
+        //}
+        //static void CalculateDiv(Func<int, int, int> divFunc, int firstNum, int secondNum)
+        //{
+        //    Console.WriteLine($"\n Reminder of {firstNum} and {secondNum} is : {divFunc(firstNum, secondNum)}");
+        //}
+    }
+}
